e79a83724043f9cacdeb4fb93433433933dfc362
maliha-afk <24amaliha@gmail.com>
2024-09-15 21:39:26 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import datetime
+
+#Date= datetime.datetime.now()
+#print(Date)
+
+#Today= datetime.date.today()
+#print(Today)
+
+#Time= datetime.datetime.now().time()
+#print(Time)
+
+#T= datetime.datetime.now().time()
+#DT=datetime.datetime.now()
+#T2=datetime.date.now().today
+#print(DT)
+#print(T)
+#print(T2)
+
+import time
+ST=datetime.datetime.now().time()
+print(ST)
+time.sleep(2)
+ET=datetime.datetime.now().time()
+print(ET)
